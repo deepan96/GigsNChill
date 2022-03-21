8d14b70db14ccf62f4b304777bb6163c3b7c1f0e
Deepan Elangovan <deepanelangovan@Deepans-MacBook-Pro.local>
2022-03-20 22:24:47 -0400

    Resolving conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,28 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import Login from './Components/LandingPages/Login';
-import SignUp from './Components/LandingPages/SignUp';
-import LandingPage from './Components/LandingPages/LandingPage';
-import Home from './Components/LandingPages/Home';
 import {Routes, Route} from 'react-router-dom';
-import ForgotPassword from './Components/LandingPages/ForgotPassword';
+import { LoginStateContext, UserEmailStateConext } from './Components/Context';
+import { RouteStack,LoginRouteStack } from './Components/Auth/RouteStack';
+import MainHeader from './Components/UI/Header/MainHeader';
 function App() {
-  
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userEmail, setUserEmail] = useState(false);
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<LandingPage/>} />
-        <Route path="/login" element={<Login/>} />
+      <LoginStateContext.Provider value = {{isLoggedIn, setIsLoggedIn}}>
+        
+        <MainHeader/>
+        {console.log(isLoggedIn)}
+        {!isLoggedIn ? RouteStack(): LoginRouteStack()}
+      {/* <Routes>
+        <Route path="/*" element={<LandingPage/>} />
+        {/* <Route path="/login" element={<Login/>} />
         <Route path="/signup" element={<SignUp/>} />
         <Route path="/forgotpassword" element={<ForgotPassword/>} />
-        <Route path="/home" element={<Home/>} />
-      </Routes>
+        <Route path='/resetpassword' element={<ResetPassword/>}/> */}
+        {/* <Route path="/home" element={<Home/>} /> */}
+      {/* </Routes> */}
+      </LoginStateContext.Provider>
     </div>
   );
 }
